a95b16a7f92c498f92bc3d6ed95cf67b7463a33b
genadi60 <genadimihaylov@gmail.com>
2018-03-11 03:06:07 +0200

    Adding missed out files.

---
+++ BashSoft/StoryMode/StoryMode/BashSoft/IO/OutputWriter.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace BashSoft
+{
+    using System;
+    using System.Collections.Generic;
+
+    static class OutputWriter
+    {
+        static OutputWriter()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+        }
+
+        public static void WriteMessage(string message)
+        {
+            Console.Write(message);
+        }
+
+        public static void WriteMessageOnNewLine(string message)
+        {
+            Console.WriteLine(message);  
+        }
+
+        public static void WriteEmptyLine()
+        {
+            Console.WriteLine();
+        }
+
+        public static void PrintStudent(KeyValuePair<string, List<int>> student)
+        {
+            OutputWriter.WriteMessageOnNewLine(string.Format($"{student.Key} - {string.Join(", ", student.Value)}"));
+        }
+
+        public static void DisplayException(string message)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = currentColor;
+        }
+    }
+}
